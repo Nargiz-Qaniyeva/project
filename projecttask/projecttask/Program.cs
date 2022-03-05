0575d9c8b2cdfc75d78e30ea3d6b8d5fbbea5293
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-05 17:51:51 +0400

    first commit

---
+++ projecttask/projecttask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace projecttask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, count = 0;
+            while (true)
+            {
+                Console.WriteLine("eded daxil et:");
+                number = Convert.ToInt32(Console.ReadLine());
+                {
+                    while (number > 0)
+                    {
+                        number = number / 10;
+                        count++;
+                    }
+                    Console.WriteLine("merteblerin  sayi:" + count);
+                    Console.ReadLine(); 
+                }
+            }
+        }
+    }
+}
